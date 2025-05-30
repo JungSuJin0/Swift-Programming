18b92f8b1ff64b8ebffaeebda59bc4101b42617e
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-30 14:32:15 +0900

    Add files via upload

---
+++ Chapter06. 흐름제어/6-17. while 반복 구문의 사용.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var names: [String] = ["Joker", "Jenny", "Nova", "yagom"]
+
+while names.isEmpty == false {
+    print("Good bye \((names.removeFirst()))")
+    //removeFirst()는 요소를 삭제함과 동시에 삭제한 요소를 반환함
+}
+
+//Good bye Joker
+//Good bye Jenny
+//Good bye Nova
+//Good bye yagom
