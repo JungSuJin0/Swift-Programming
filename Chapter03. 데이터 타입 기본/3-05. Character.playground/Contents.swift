99feecaca333217de500c2adc5c06a9d1d87706c
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-31 17:19:14 +0900

    Update Contents.swift

--- Chapter03. 데이터 타입 기본/3-05. Character.playground/Contents.swift
+++ Chapter03. 데이터 타입 기본/3-05. Character.playground/Contents.swift
@@ -1,20 +1,12 @@
 import UIKit
 
 let alphabetA: Character = "A"
-print(alphabetA)
-
-//A
-
+print(alphabetA)  //A
 
 //Character 값에 유니코드 문자를 사용할 수 있음
 let commandCharacter: Character = "♥"
-print(commandCharacter)
-
-//♥
-
+print(commandCharacter)  //♥
 
 let 한글변수이름: Character = "ㄱ"
 //한글 유니코드 문자에 속하므로 스위프트 코드의 변수 이름으로 사용할 수 있음
-print("한글의 첫 자음: \(한글변수이름)")
-
-//한글의 첫 자음: ㄱ
+print("한글의 첫 자음: \(한글변수이름)")  //한글의 첫 자음: ㄱ
