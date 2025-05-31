836d3ee332537b7f1ea5e1408f64278035373425
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-31 14:35:33 +0900

    Update Contents.swift

--- Chapter02. 스위프트 처음 시작하기/2-07. 상수의 선언 및 사용.playground/Contents.swift
+++ Chapter02. 스위프트 처음 시작하기/2-07. 상수의 선언 및 사용.playground/Contents.swift
@@ -1,10 +1,9 @@
 import UIKit
 
-let name: String = "yagom"
+let name: String = "수진"
 var age: Int = 100
 var job = "iOS Programmer"
-let height =  181.5
+let height =  156
 age = 99  //변수의 값은 변경할 수 있음
-job = "Writer"
-//name = "야곰"  //상수로 선언된 값은 변경할 수 없음. 오류 발생
-print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 작업은 \(job)입니다. 비밀이지만, 키는 \(height) 센티미터 입니다.")
+//height = 160  //상수의 값은 변경할 수 없음
+print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 작업은 \(job)입니다. 비밀이지만, 키는 \(height)센티미터 입니다.")
