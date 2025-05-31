990104d2408a7366d4b0585005157a0f7f05784e
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-31 17:28:17 +0900

    Update Contents.swift

--- Chapter02. 스위프트 처음 시작하기/2-06. 변수의 선언 및 사용.playground/Contents.swift
+++ Chapter02. 스위프트 처음 시작하기/2-06. 변수의 선언 및 사용.playground/Contents.swift
@@ -4,8 +4,7 @@ var name: String = "수진"
 var age: Int = 100
 var job = "iOS programmer"  //타입 추론이 사용
 var height =  156
-print("\(type(of: height))")
-
+print("\(type(of: height))")  //Int
 age = 24  //변수는 값을 변경해줄 수 있음
 job = "개발자"  //값을 변경할 때는 기존과 같은 타입의 값을 할당해주어야 함
 print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 작업은 \(job)입니다. 비밀이지만, 키는 \(height)센티미터 입니다.")
