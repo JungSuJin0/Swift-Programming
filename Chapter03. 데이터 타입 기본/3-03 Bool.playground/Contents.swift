644785ec983fc0ef24ba0cd1381ffb39cb016a2b
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-31 17:15:33 +0900

    Update Contents.swift

--- Chapter03. 데이터 타입 기본/3-03 Bool.playground/Contents.swift
+++ Chapter03. 데이터 타입 기본/3-03 Bool.playground/Contents.swift
@@ -4,6 +4,4 @@ var boolean: Bool = true
 boolean.toggle()  //true - false 반전
 let iLoveYou: Bool = true
 let isTimeUnlimited: Bool = false
-print("시간은 무한합니까?: \(isTimeUnlimited)")
-
-//시간은 무한합니까?: false
+print("시간은 무한합니까?: \(isTimeUnlimited)")  //시간은 무한합니까?: false
