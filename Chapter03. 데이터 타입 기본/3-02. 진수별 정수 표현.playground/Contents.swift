74d740997f997dd5383a17f434ab307441757656
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-31 16:23:04 +0900

    Add files via upload

---
+++ Chapter03. 데이터 타입 기본/3-02. 진수별 정수 표현.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let decimalInteger: Int = 28
+let binaryInteger: Int = 0b11100  //2진수로 10진수 28을 표현
+let octalInteger: Int = 0o34  //8진수로 10진수를 28로 표현
+let hexadecimalInteger: Int = 0x1c  //16진수로 10진수 28을 표현
---
+++ Chapter03. 데이터 타입 기본/3-03 Bool.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var boolean: Bool = true
+boolean.toggle()  //true - false 반전
+let iLoveYou: Bool = true
+let isTimeUnlimited: Bool = false
+print("시간은 무한합니까?: \(isTimeUnlimited)")
+
+//시간은 무한합니까?: false
