ad13c9f076885a61814432a6ded4e4b52351f2c8
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-06-05 17:40:43 +0900

    Update Contents.swift

--- Chapter02. 스위프트 처음 시작하기/2-07. 상수의 선언 및 사용.playground/Contents.swift
+++ Chapter02. 스위프트 처음 시작하기/2-07. 상수의 선언 및 사용.playground/Contents.swift
@@ -4,8 +4,8 @@ let name: String = "수진"
 var age: Int = 100
 var job = "iOS programmer"
 let height = 156
-age = 99  //변수의 값은 변경할 수 있음
+age = 24  //변수의 값은 변경할 수 있음
 //height = 160  //상수의 값은 변경할 수 없음
 
 print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 작업은 \(job)입니다. 비밀이지만, 키는 \(height)센티미터 입니다.")
-//저의 이름은 수진이고, 나이는 99세이며, 작업은 iOS programmer입니다. 비밀이지만, 키는 156센티미터 입니다.
+//저의 이름은 수진이고, 나이는 24세이며, 작업은 iOS programmer입니다. 비밀이지만, 키는 156센티미터 입니다.
