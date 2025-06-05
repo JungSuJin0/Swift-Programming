2c02162184d8d3462bc78d8462002aa09b99c9c5
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-06-05 17:29:14 +0900

    Update Contents.swift

--- Chapter02. 스위프트 처음 시작하기/2-01. 문자열 보간법 사용.playground/Contents.swift
+++ Chapter02. 스위프트 처음 시작하기/2-01. 문자열 보간법 사용.playground/Contents.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 let name: String = "수진"
-print("My name is \(name)")
-print("My naem is", name)
+print("My name is \(name)")  //My name is 수진
+print("My naem is", name)  //My name is 수진
 
-//My name is 수진
-//My name is 수진
+//줄바꿈 문자
+print("My name is \n \(myname)")  //My name is
+                                  //수진
