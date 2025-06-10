0dcdc328773c85a4327768ccd7a3686d773dc26c
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-06-10 13:31:58 +0900

    Update Contents.swift

--- Chapter04. 데이터 타입 고급/4-02. 타입 별칭.playground/Contents.swift
+++ Chapter04. 데이터 타입 고급/4-02. 타입 별칭.playground/Contents.swift
@@ -11,4 +11,4 @@ var year: YourInt = 2080  //YourInt도 Int의 또 다른 이름
 year = age
 
 let month: Int = 7  //물론 기존의 Int도 사용 가능
-let percentage:MyDouble = 99.9  //Int 외에 다른 자료형도 모두 별칭 사용이 가능
+let percentage: MyDouble = 99.9  //Int 외에 다른 자료형도 모두 별칭 사용이 가능
