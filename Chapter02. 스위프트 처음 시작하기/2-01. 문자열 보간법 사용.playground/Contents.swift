d6647a7f1f91549bba21e85f195616045206f246
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-06-10 09:49:20 +0900

    Update Contents.swift

--- Chapter02. 스위프트 처음 시작하기/2-01. 문자열 보간법 사용.playground/Contents.swift
+++ Chapter02. 스위프트 처음 시작하기/2-01. 문자열 보간법 사용.playground/Contents.swift
@@ -5,5 +5,5 @@ print("My name is \(name)")  //My name is 수진
 print("My naem is", name)  //My name is 수진
 
 //줄바꿈 문자
-print("My name is \n \(myname)")  //My name is
+print("My name is \n \(name)")  //My name is
                                   //수진
